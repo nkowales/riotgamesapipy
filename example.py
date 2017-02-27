51eac0cbaac48238b96f7c6e30bb1f0c8b4f063d
Alanna Lynn McEachen <amceache@student00.cse.nd.edu>
2017-02-27 12:58:41 -0500

    changed example.py a wee bit

--- example.py
+++ example.py
@@ -3,7 +3,8 @@ from info import API_KEY
 
 def main():
 	rito = riotgamesapipy.riotgamesapipy(API_KEY)
-	c = rito.getChampionMastery(43103897, 36)
+	#c = rito.getChampionMastery(43103897, 36)
+	c = rito.getPlayerRecentGames(43103897)
 	print c
 
 if __name__ == "__main__":
