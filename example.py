0ff75c9e1a26dbbd0147d002226ad6e6f1f4892e
nkowales <nkowales@nd.edu>
2017-02-27 15:32:00 -0500

    changed example name

--- example.py
+++ example.py
@@ -3,17 +3,17 @@ from info import API_KEY
 
 def main():
 	rito = riotgamesapipy.riotgamesapipy(API_KEY)
-	c = rito.getSummonerbyName('Dairus')
-	print "Looking up stats for player 'Dairus', ID:", c["dairus"]["id"]
+	c = rito.getSummonerbyName('Dyrus')
+	print "Looking up stats for player 'Dyrus', ID:", c["dyrus"]["id"]
 
-	idnum = c["dairus"]["id"]
+	idnum = c["dyrus"]["id"]
 	league = rito.getLeagueEntries(idnum)
-	#print league[str(c["dairus"]["id"])]
+	#print league[str(c["dyrus"]["id"])]
 	print "Tier:", league[str(idnum)][0]["tier"], league[str(idnum)][0]["entries"][0]["division"]
 	print "Wins / Losses: ", league[str(idnum)][0]["entries"][0]["wins"], "/", league[str(idnum)][0]["entries"][0]["losses"]
 	
 	masteries = rito.getTopMasteries(str(idnum), 10)
-	print "Darius' Top 10 Masteries:"
+	print "Dyrus' Top 10 Masteries:"
 	for entry in masteries:
 		champ = rito.getChampionDataByID(entry["championId"])
 		print "Name:", champ["name"], "\t\t", "Level:", entry["championLevel"], "\t", "Points towards Mastery:", entry["championPoints"]
