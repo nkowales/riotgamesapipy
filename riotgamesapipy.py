12880df96c98db550195ef2cc6b06325dae3d0d3
Alanna Lynn McEachen <amceache@student00.cse.nd.edu>
2017-03-20 13:33:46 -0400

    added some summoner methods, cleaned up other places

--- riotgamesapipy.py
+++ riotgamesapipy.py
@@ -9,6 +9,7 @@ class riotgamesapipy:
 		self.region = region
 		self.server = server
 
+
 	### CHAMPION ###
 	def getChampions(self, ftp = False):
 	# all the league champions
@@ -24,32 +25,37 @@ class riotgamesapipy:
 		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.2/champion/' + str(championID) + '?api_key='+ self.api_key)
 		champ = r.json()
 		return champ
+
 		
 	### CHAMPION MASTERY ###
 	def getPlayerMasteries(self, summonerID):
 	# get that summoner's champion masteries, search by ID
-		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.region + '1/player/' + str(summonerID) + '/champions?api_key=' + self.api_key)
+		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.server + '/player/' + str(summonerID) + '/champions?api_key=' + self.api_key)
 		masteries = r.json()
 		return masteries
+
 
 	def getChampionMastery(self, summonerID, championID):
 	# get summoner's mastery of a given champion, search by summoner and
 	# champion IDs
-		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.region + '1/player/' + str(summonerID) + '/champion/' + str(championID) + '?api_key=' + self.api_key)
+		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.server + '/player/' + str(summonerID) + '/champion/' + str(championID) + '?api_key=' + self.api_key)
 		mastery = r.json()
 		return mastery
 
+
 	def getMasteryScore(self, summonerID):
 	# get the total number of summoner mastery points, int
-		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.region + '1/player/' + str(summonerID) + '/score?api_key=' + self.api_key)
+		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.server + '/player/' + str(summonerID) + '/score?api_key=' + self.api_key)
 		score = r.json()
 		return int(score)
 
+
 	def getTopMasteries(self, summonerID, retrieve):
 	# get the player's champions w highest masteries
-		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.region + '1/player/' + str(summonerID) + '/topchampions?count=' + str(retrieve) + '&api_key=' + self.api_key)
+		r = requests.get('https://' + self.region + '.api.pvp.net/championmastery/location/' + self.server + '/player/' + str(summonerID) + '/topchampions?count=' + str(retrieve) + '&api_key=' + self.api_key)
 		top = r.json()
 		return top
+
 
 	### CURRENT GAME ###
 	def getCurrentGame(self, summonerID):
@@ -58,12 +64,14 @@ class riotgamesapipy:
 		cgame = r.json()
 		return cgame
 
+
 	### FEATURED GAMES ###
 	def getFeaturedGames(self):
 	# get info on the highest ranking current games
 		r = requests.get('https://' + self.region + '.api.pvp.net/observer-mode/rest/featured?api_key=' + sefl.api_key)
 		fgames = r.json()
 		return fgames
+
 
 	### GAME ###
 	def getPlayerRecentGames(self, summonerID):
@@ -72,32 +80,34 @@ class riotgamesapipy:
 		rgames = r.json()
 		return rgames
 
+
 	### LEAGUES ###
 	def getLeagues(self, summonerIDs):
-		# get info on the leagues that a given summoner is a member of. Takes
-		# comma-separated summoner ids string
-		# excludes inactive teams and players except players in the input list
+	# get info on the leagues that a given summoner is a member of. Takes
+	# comma-separated summoner ids string
+	# excludes inactive teams and players except players in the input list
 		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v2.5/league/by-summoner/' + str(summonerIDs) + '?api_key=' + self.api_key)
 		leagues = r.json()
 		return leagues
 
 	def getLeagueEntries(self, summonerIDs):
-		#gets info on leages summoner is a member of. Takes comma separated string of ids		
+	#gets info on leages summoner is a member of. Takes comma separated string of ids		
 		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v2.5/league/by-summoner/' + str(summonerIDs) + '/entry?api_key=' + self.api_key)
 		leagues = r.json()
 		return leagues
 		
 	def	getChallenger(self, Qname):
-		#return challenger league for given ranked queue, RANKED_FLEX_SR, RANKED_FLEX_TT, RANKED_SOLO_5x5, RANKED_TEAM_5x5, or RANKED_TEAM_3x3, 
+	#return challenger league for given ranked queue, RANKED_FLEX_SR, RANKED_FLEX_TT, RANKED_SOLO_5x5, RANKED_TEAM_5x5, or RANKED_TEAM_3x3, 
 		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v2.5/league/challenger?type=' + Qname + '&api_key=' + self.api_key)
 		league = r.json()
 		return league
 		
 	def	getMaster(self, Qname):
-		#return Master league for given ranked queue, RANKED_FLEX_SR, RANKED_FLEX_TT, RANKED_SOLO_5x5, RANKED_TEAM_5x5, or RANKED_TEAM_3x3, 
+	#return Master league for given ranked queue, RANKED_FLEX_SR, RANKED_FLEX_TT, RANKED_SOLO_5x5, RANKED_TEAM_5x5, or RANKED_TEAM_3x3, 
 		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v2.5/league/master?type=' + Qname + '&api_key=' + self.api_key)
 		league = r.json()
 		return league
+
 		
 	### LoL STATIC DATA ###
 	#def getChampionData
@@ -187,6 +197,7 @@ class riotgamesapipy:
 		shards = r.json()
 		return shards
 		
+
 	### MATCH ###
 	def getMatch(self, matchID, timeline = False):
 		#returns match by match ID, with timeline tag indicating whether to include timeline data or not
@@ -197,26 +208,25 @@ class riotgamesapipy:
 		match = r.json()
 		return match
 	
+
 	### MATCHLIST ###
         def getMatchList(self, summonerID, rankedQueues=None, endTime=None, endIndex=None, beginTime=None, beginIndex=None, seasons=None, championIds=None):
                 options=''
                 if rankedQueues:
-                        options = options + 'rankedQueues=' + rankedQueues + '&'
+                        options = options + 'rankedQueues=' + str(rankedQueues) + '&'
                 if endTime:
-                        options = options + 'endTime=' + endTime + '&'
+                        options = options + 'endTime=' + str(endTime) + '&'
                 if endIndex:
-                        options = options + 'endIndex=' + endIndex + '&'
+                        options = options + 'endIndex=' + str(endIndex) + '&'
                 if beginTime:
-                        options = options + 'beginTime=' + beginTime + '&'
+                        options = options + 'beginTime=' + str(beginTime) + '&'
                 if beginIndex:
-                        options = options + 'beginIndex=' + beginIndex + '&'
+                        options = options + 'beginIndex=' + str(beginIndex) + '&'
                 if seasons:
                         options = options + 'seasons=' + str(seasons) + '&'
                 if championIds:
-                        options = options + 'championIds=' + championIds + '&'
+                        options = options + 'championIds=' + str(championIds) + '&'
                 
-                # use None?
-                #r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v2.2/matchlist/by-summoner' + str(summonerID) + '?api_key=' + self.api_key)
                 r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v2.2/matchlist/by-summoner/' + str(summonerID) + '?' + options + 'api_key=' + self.api_key)
                 matchlist = r.json()
                 return matchlist
@@ -233,6 +243,7 @@ class riotgamesapipy:
 		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.3/stats/by-summoner/'+ summonerID + '/summary?season=' + season + '&api_key=' + self.api_key)
 		stats = r.json()
 		return stats
+
 	
 	### SUMMONER ###
 	def getSummonerbyName(self, summonerN):
@@ -240,12 +251,29 @@ class riotgamesapipy:
 		summoner = r.json()
 		return summoner
 		
-	#def getSummonersByID(self):
-	
-	#def getSummonerNamesByID(self):
-	
-	#def getSummonerMasteries(self, summonerID):
-	
-	#def getSummonerRunes(self, summonerID):
+	def getSummonersByID(self, summonerIDs):
+		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.4/summoner/' + str(summonerID) + '?api_key=' + self.api_key)
+		summoners = r.json()
+		return summoners 
+
+	def getSummonersByAcctID(self, accountIDs):
+		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.4/summoner/by-account/' + str(accountID) +'?api_key=' + self.api_key)
+		summoners = r.json()
+		return summoners 
+
+	def getSummonerNameByID(self, summonerID):
+		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.4/summoner/' + str(summonerID) + '/name?api_key=' + self.api_key)
+		name = r.json()
+		return name
+
+	def getSummonersMasteries(self, summonerIDs):
+		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.4/summoner/' + str(summonerIDs) + '/masteries?api_key=' + self.api_key)
+		masteries = r.json()
+		return masteries
+
+	def getSummonersRunes(self, summonerID):
+		r = requests.get('https://' + self.region + '.api.pvp.net/api/lol/' + self.region + '/v1.4/summoner/' + str(summonerIDs) + '/runes?api_key=' + self.api_key)
+		runes = r.json()
+		return runes
 
 	### PROJECT SPECIFIC ###
