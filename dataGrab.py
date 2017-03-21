a6ccd59a21b641877dbce26101abba32a61c6e39
Alanna Lynn McEachen <amceache@student00.cse.nd.edu>
2017-03-21 19:08:10 -0400

    should in theory grab, sort, and print the relevant data.

--- dataGrab.py
+++ dataGrab.py
@@ -1,4 +1,5 @@
 import riotgamesapipy
+import time
 from info import API_KEY
 
 def main():
@@ -7,68 +8,67 @@ def main():
 	matches=[] # list of games we got info from
 
 	# for non-challenger entries - 
-	bronzechamp = rito.getLeagues(37517949)
-	bronze = bronzechamp["37517949"][0]
-	silverchamp = rito.getLeagues(51575588)
-	silver = silverchamp["51575588"][0]
-	goldchamp = rito.getLeagues(35707215)
-	gold = goldchamp["35707215"][0]
-	platinumchamp = rito.getLeagues(31593648)
-	platinum = platinumchamp["31593648"][0]
+	#bronzechamp = rito.getLeagues(37517949)
+	#bronze = bronzechamp["37517949"][0]
+	#silverchamp = rito.getLeagues(51575588)
+	#silver = silverchamp["51575588"][0]
+	#goldchamp = rito.getLeagues(35707215)
+	#gold = goldchamp["35707215"][0]
+	#platinumchamp = rito.getLeagues(31593648)
+	#platinum = platinumchamp["31593648"][0]
 
 	master = rito.getMaster('RANKED_SOLO_5x5')
-	challenger = rito.getChallenger('RANKED_SOLO_5x5')
+	#challenger = rito.getChallenger('RANKED_SOLO_5x5')
 
-        for entry in platinum["entries"]: # replace "challenger" with other league
+        for entry in master["entries"]: # replace "challenger" with other league
                 matchlist = rito.getMatchList(str(entry["playerOrTeamId"]), seasons='SEASON2016,PRESEASON2017')
                 for matl in matchlist["matches"]:
                         matchID = matl["matchId"]
+			role = [None] * 10
 			if matchID not in matches:
+				sleep(1)
 				matches.append(matchID)
-				print matchID
                         	match = rito.getMatch(matchID, timeline=True)
 	                        for part in match["participants"]:
-			#		grabStats(rito, part)
-					champ = rito.getChampionDataByID(part["championId"])
 					if part["timeline"]["lane"] == "BOTTOM" and part["timeline"]["role"] == "DUO_CARRY":
-				                role = "ADCARRY"
+						if part["teamId"] == 0:
+							print part["championId"]
+							role[6] = str(part["championId"])
+							print role[6]
+						else:
+							print part["championId"]
+
+							role[7] = str(part["championId"])
+							print role[7]
+							print role[6], "what"
 					elif part["timeline"]["lane"] == "BOTTOM" and part["timeline"]["role"] == "DUO_SUPPORT":
-                				role = "SUPPORT"
-				        else:
-                				role = part["timeline"]["lane"]
-					print role, champ["name"], part["stats"]["winner"]
-                                
-					# rival champ stats!
-                        		#for part2 in match["participants"]:
-                                	#	if part["timeline"]["lane"] == part2["timeline"]["lane"] and part["timeline"]["role"] == part2["timeline"]["role"] and part["participantId"] != part2["participantId"]:
-                                        #	        print "Rival Stats!!"
-                                         #       	grabStats(rito, part2)
-                
+						if part["teamId"] == 0:
+							role[8] = str(part["championId"])
+						else:
+							role[9] = str(part["championId"])
 
+				        elif part["timeline"]["lane"] == "TOP":
+						if part["teamId"] == 0:
+							role[0] = str(part["championId"])
+						else:
+							role[1] = str(part["championId"])
+					elif part["timeline"]["lane"] == "JUNGLE":
+						if part["teamId"] == 0:
+							role[2] = str(part["championId"])
+						else:
+							role[3] = str(part["championId"])
+					elif part["timeline"]["lane"] == "MIDDLE":
+						if part["teamId"] == 0:
+							role[4] = str(part["championId"])
+						else:
+							role[5] = str(part["championId"])
+				if match["teams"][0]["winner"]:
+					w="1"
+				else:
+					w="0"
+				print matchID, role[0], role[1], role[2], role[3], role[4], role[5], role[6], role[7], role[8], role[9], w
+				#dbfuncs.insertgame(matchID, top1, top2, jun1, jun2, mid1, mid2, bot1, bot2, sup1, sup2, w)
 
-
-def grabStats(rito, part):
-        champ = rito.getChampionDataByID(part["championId"])
-        print champ["name"]
-        # get the champion's role
-        if part["timeline"]["lane"] == "BOTTOM" and part["timeline"]["role"] == "DUO_CARRY":
-                print "ADCARRY"
-        elif part["timeline"]["lane"] == "BOTTOM" and part["timeline"]["role"] == "DUO_SUPPORT":
-                print "SUPPORT"
-        else:
-                print part["timeline"]["lane"]
-        # get some general statistics for that champion
-        # lots of good stuff under "timeline" (e.g. gold diff, cs diff)
-        # but i'm leaving that out for right now.
-        print part["stats"]["winner"]
-        print part["stats"]["kills"]
-        print part["stats"]["assists"]
-        print part["stats"]["deaths"]
-        print part["stats"]["physicalDamageDealt"]
-        print part["stats"]["magicDamageDealt"]
-        print part["stats"]["goldEarned"]
-        print part["stats"]["totalDamageTaken"]
-        print part["stats"]["totalHeal"] # i assume healing others? healing done to allies, including self, excluding health regeneration and lifesteal
 
 
 if __name__ == "__main__":
