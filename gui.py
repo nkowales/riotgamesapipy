cb5ef85b7de76057819d7fa14f4ec37b3886116e
nkowales <nkowales@nd.edu>
2017-03-28 10:33:58 -0400

    added exception handling to gui

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 from Tkinter import *
+from id2name import NAME2ID
 import tkMessageBox
 import dbfuncs
+pos = ['supp', 'bot', 'mid', 'jungle', 'top']
+posnn = {'support' : 'supp', 'sup' : 'supp', 'adc' : 'bot', 'bottom' : 'bot', 'middle' : 'mid', 'solomid' : 'mid', 'jung' : 'jungle', 'jun' : 'jungle', 'j' : 'jungle'}
 top = Tk()
 F0 = Frame(top)
 F1 = Frame(F0)
@@ -11,11 +14,21 @@ F1.pack( side = TOP )
 F2.pack()
 F3.pack( side = BOTTOM )
 def helloCallBack():
-	#try:
-	results = dbfuncs.suggestion(E2.get(), E1.get(), E3.get())
-	tkMessageBox.showinfo( "Suggested Champions", str(results))
-	#except:
-		#tkMessageBox.showinfo("Suggested Champions", "Error")
+	try:
+		if E2.get() not in NAME2ID:
+			raise Exception('invalid champion name')
+		if E3.get().lower() not in pos:
+			if E3.get().lower() not in posnn:
+				raise Exception('invalid position')
+			else:
+				postemp = posnn[E3.get().lower()]
+		else:
+			postemp = E3.get().lower()
+		results = dbfuncs.suggestion(E2.get(), E1.get(), postemp)
+		tkMessageBox.showinfo( "Suggested Champions", str(results))
+		
+	except Exception as e:
+		tkMessageBox.showinfo("Suggested Champions", str(e))
 
 L1 = Label(F1, text="Summoner Name")
 L1.pack( side = LEFT)
